f6c38c46948eac12711e3f34847c15b7c5c42a4c
rohit0110 <rohit7rathore15@gmail.com>
2022-10-15 16:19:28 +0530

    Basic layout added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/responsive/mobile_layout.dart';
 import 'package:insta_clone/responsive/responsive_layout.dart';
 import 'package:insta_clone/responsive/web_layout.dart';
 import 'package:insta_clone/utils/colors.dart';
+import 'package:insta_clone/views/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyDj5zzL-YKB6n5YlKn4OxVANsNsJClb-Og',
+          appId: '1:55410437694:web:033cd10e0497714a02ab6f',
+          messagingSenderId: '55410437694',
+          projectId: 'insta-clone-96feb',
+          storageBucket: 'insta-clone-96feb.appspot.com'),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -18,14 +32,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Instagram Clone in Flutter',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(
-        mobileScreenLayout: MobileLayout(),
-        webScreenLayout: WebLayout(),
-      ),
+      // home: const ResponsiveLayout(
+      //   mobileScreenLayout: MobileLayout(),
+      //   webScreenLayout: WebLayout(),
+      //),
+      home: const LoginScreen(),
     );
   }
 }
--- lib/responsive/mobile_layout.dart
+++ lib/responsive/mobile_layout.dart
@@ -5,6 +5,10 @@ class MobileLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text("This is mobile"),
+      ),
+    );
   }
 }
